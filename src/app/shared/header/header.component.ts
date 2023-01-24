4999327f6929ee17e44f3fd74d42c28e443e5344
abdallah bou <abdallah.boukfou@labom2iformation.fr>
2023-01-24 12:33:51 +0100

    header lien ngIf ok

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from 'src/app/service/header.service';
+import { StorageService } from 'src/app/service/storage.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,24 @@ import { HeaderService } from 'src/app/service/header.service';
 })
 export class HeaderComponent implements OnInit {
   titre : string = "";
-  constructor(private headerService : HeaderService) { }
+  type: any
+  constructor(private headerService : HeaderService,private storageService :  StorageService) {
+
+    this.type=this.storageService.get()
+    if(this.type == "passager"){
+      console.log("passager");
+      this.headerService.changeTitre(this.titre)
+
+    }else if(this.type == "conducteur"){
+      console.log("conducteur");
+      this.headerService.changeTitre(this.titre)
+
+    }else{
+      console.log("rien");
+
+    }
+
+   }
 
   ngOnInit(): void {
     this.headerService.change.subscribe((x)=>{
